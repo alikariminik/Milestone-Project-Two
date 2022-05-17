c759ead3a7972e11d6120a23f453ab8efb7ebc65
alikariminik <alirkariminik@gmail.com>
2022-05-17 18:13:37 +0000

    add javascript and first function

---
+++ script.js
@@ -0,0 +1,11 @@
+let launchQuestions = document.getElementById('launch-questions');
+launchQuestions.addEventListener('click', startQuiz);
+
+let imageGenerationField = document.getElementsByClassName(image-generation-field);
+
+
+function startQuiz (event) {
+    let newHeading = document.createElement("h3");
+    newHeading.innerHTML = "Mountains or Beaches?";
+    console.log("hello")
+}
