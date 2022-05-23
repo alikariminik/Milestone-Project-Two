14f210fb22ba0d29329705177c681a3e90ef291a
alikariminik <alirkariminik@gmail.com>
2022-05-23 20:06:37 +0000

    button now removes introductory text

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 let launchQuestions = document.getElementById('launch-questions');
 launchQuestions.addEventListener('click', startQuiz);
 
-let pictureGenerationField = document.getElementsByClassName(picture-generation-field);
+let questionGenerationField = document.getElementsByClassName(question-generation-field);
 
 
 function startQuiz (event) {
     let button = document.getElementsByTagName("button")[0] 
     button.remove();
-    $("#picture-generation-field").html("<p>Beach or Mountains?</p>").addClass("test");
+    let intro = document.getElementsByClassName("introduction")[0] 
+    intro.remove();
+    $("#question-generation-field").html("<p>Beach or Mountains?</p>").addClass("test");
     /*let launchQuestions = document.getElementById('launch-questions');
     launchQuestions.innerHTML += "Mountains or Beaches?";
     let newHeading = document.createElement("h3");
