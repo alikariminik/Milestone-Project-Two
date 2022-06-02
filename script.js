2c8f447d0b6bb97effc0b1a8f8d7dc6486c81682
alikariminik <alirkariminik@gmail.com>
2022-06-02 14:54:57 +0000

    added unsplash api key and options

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+const UNSPLASH_API_KEY = "Gu2n4WYUlx5onlhkP2AvCODE2KHAO7eNX2noQdq2Tk0";
+
+const OPTION_A_ID = "optionA";
+const OPTION_B_ID = "optionB";
+
+const OPTIONS = [
+    {a: "beach", b: "mountain range"},
+    {a: "city", b: "rural"},
+    {a: "adventure", b: "relaxation"},
+    {a: "history", b: "nightlight"},
+    {a: "culture", b: "nature"},
+]
+
 let launchQuestions = document.getElementById('launch-questions');
 launchQuestions.addEventListener('click', startQuiz);
 
