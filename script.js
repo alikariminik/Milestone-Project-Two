3829a020875f8b372763a8f72c5607547dcc158c
alikariminik <alirkariminik@gmail.com>
2022-06-02 12:27:06 +0000

    removed commented out  javascript code. no longer needed

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 let launchQuestions = document.getElementById('launch-questions');
 launchQuestions.addEventListener('click', startQuiz);
 
-let questionGenerationField = document.getElementsByClassName(question-generation-field);
+let questionGenerationField = document.getElementById(question-generation-field);
 
 
 function startQuiz (event) {
     let intro = document.getElementsByClassName("introduction")[0] 
     intro.remove();
-    /*let wastefulRealEstate = document.getElementsByClassName("launch-positioning")[0]
-    wastefulRealEstate.remove(); */
     let button = document.getElementsByTagName("button")[0] 
     button.remove();
     $("#question-generation-field").html("<p class='test'>Beaches or Mountains?</p>");
@@ -16,10 +14,6 @@ function startQuiz (event) {
     $("#op-a").addClass("left-image-wrapper");
     $("#op-b").addClass("right-image-wrapper");
     $("#reset-button").html("<button class='reset-button'>start again <i class='fa-solid fa-arrow-rotate-right'></i> </button>");
-    /*let launchQuestions = document.getElementById('launch-questions');
-    launchQuestions.innerHTML += "Mountains or Beaches?";
-    let newHeading = document.createElement("h3");
-    div[1].appendChild(newHeading);*/
 }
 
 function startAgain (event) {
