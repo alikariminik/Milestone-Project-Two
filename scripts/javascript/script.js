105dcd46fbc90861fb55981ffc140be5b810c7ef
alikariminik <alirkariminik@gmail.com>
2022-06-03 14:38:56 +0000

    added IDs from TomTom API

--- scripts/javascript/script.js
+++ scripts/javascript/script.js
@@ -5,7 +5,7 @@ const OPTION_B_ID = "optionB";
 
 const OPTIONS = [{
         a: { name: "beach", id: ["9357", "9937002"]}, //[Beach, Beach Club]
-        b: { name: "mountain range", id: ["7302002", "8099002", "9935", 9362026]}, //[Mountain Bike Trail, Mountain Peak, Mountain Pass, Ski Resort]
+        b: { name: "mountain range", id: ["7302002", "8099002", "9935", "9362026"]}, //[Mountain Bike Trail, Mountain Peak, Mountain Pass, Ski Resort]
     },
     {
         a: "city",
@@ -13,15 +13,15 @@ const OPTIONS = [{
     },
     {
         a: { name: "adventure", id: ["7305", "9371", "7302004", "9902003"]}, //[Adventure Sports Facility, Water Sport, Hiking Trail, Amusement Park]
-        b: { name: "relaxation", id: ["9378005", "7338"]}, // [Sauna, Solarium & Massage, Swimming Pool]
+        b: { name: "relaxation", id: ["9378005", "7338", "7314005"]}, // [Sauna, Solarium & Massage, Swimming Pool, Resort]
     },
     {
-        a: {name: "history", id: ["7317"]}, //[Museum, ]
-        b: {name: "nightlight", id: []}, //
+        a: {name: "history", id: ["7317", "7376003", "9362007", "9362004", "9362002" ]}, //[Museum, Monument, Memorial, Historic Site, Battlefield ]
+        b: {name: "nightlight", id: ["9379002", "9379006", "9379004", "9379008", "9379009" ]}, // [Disco Club, Cocktail Bar, Bar, Jazz Club, Comedy Club ]
     },
     {
-        a: {name: "culture", id: ["7318005", ]} //[Theater,]
-        b: {name: "nature", id: ["9927003"]}, //[Zoo, ] 
+        a: {name: "culture", id: ["7318005", "7318004", "7339002", "7339003", "7339004", "7315" ]}, //[Theater, Opera House, Church, Mosque, Synagog, Restaurant]
+        b: {name: "nature", id: ["9927003, 9362015", "8099025", "8099013", "9362030", "8099019"]}, //[Zoo, Forest Area, Hill, Reservoir, Natural Recreation Attraction, Lagoon ] 
     },
 ]
 
