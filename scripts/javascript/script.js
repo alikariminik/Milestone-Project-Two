2e72ac61b7599259e8794a20d9c8b55e0e1ae90e
alikariminik <alirkariminik@gmail.com>
2022-06-04 10:58:03 +0000

    updated updateOptionPhotos function with searchQuery

--- scripts/javascript/script.js
+++ scripts/javascript/script.js
@@ -4,24 +4,20 @@ const OPTION_A_ID = "optionA";
 const OPTION_B_ID = "optionB";
 
 const OPTIONS = [{
-        a: { name: "beach", id: ["9357", "9937002"]}, //[Beach, Beach Club]
-        b: { name: "mountain range", id: ["7302002", "8099002", "9935", "9362026"]}, //[Mountain Bike Trail, Mountain Peak, Mountain Pass, Ski Resort]
+        a: { name: "Beaches", searchQuery:"beach resort", id: ["9357", "9937002"]}, //[Beach, Beach Club]
+        b: { name: "Mountain Range", searchQuery:"mountain range", id: ["7302002", "8099002", "9935", "9362026"]}, //[Mountain Bike Trail, Mountain Peak, Mountain Pass, Ski Resort]
     },
     {
-        a: "city",
-        b: "rural"
+        a: { name: "Adventurous", searchQuery:"adventure activities", id: ["7305", "9371", "7302004", "9902003"]}, //[Adventure Sports Facility, Water Sport, Hiking Trail, Amusement Park]
+        b: { name: "Relaxing", searchQuery:"massage", id: ["9378005", "7338", "7314005"]}, // [Sauna, Solarium & Massage, Swimming Pool, Resort]
     },
     {
-        a: { name: "adventure", id: ["7305", "9371", "7302004", "9902003"]}, //[Adventure Sports Facility, Water Sport, Hiking Trail, Amusement Park]
-        b: { name: "relaxation", id: ["9378005", "7338", "7314005"]}, // [Sauna, Solarium & Massage, Swimming Pool, Resort]
+        a: {name: "Historical", searchQuery:"historic monuments", id: ["7317", "7376003", "9362007", "9362004", "9362002" ]}, //[Museum, Monument, Memorial, Historic Site, Battlefield ]
+        b: {name: "Party", searchQuery:"nightlife", id: ["9379002", "9379006", "9379004", "9379008", "9379009" ]}, // [Disco Club, Cocktail Bar, Bar, Jazz Club, Comedy Club ]
     },
     {
-        a: {name: "history", id: ["7317", "7376003", "9362007", "9362004", "9362002" ]}, //[Museum, Monument, Memorial, Historic Site, Battlefield ]
-        b: {name: "nightlight", id: ["9379002", "9379006", "9379004", "9379008", "9379009" ]}, // [Disco Club, Cocktail Bar, Bar, Jazz Club, Comedy Club ]
-    },
-    {
-        a: {name: "culture", id: ["7318005", "7318004", "7339002", "7339003", "7339004", "7315" ]}, //[Theater, Opera House, Church, Mosque, Synagog, Restaurant]
-        b: {name: "nature", id: ["9927003, 9362015", "8099025", "8099013", "9362030", "8099019"]}, //[Zoo, Forest Area, Hill, Reservoir, Natural Recreation Attraction, Lagoon ] 
+        a: {name: "Cultural", searchQuery:"cultural arts", id: ["7318005", "7318004", "7339002", "7339003", "7339004", "7315" ]}, //[Theater, Opera House, Church, Mosque, Synagog, Restaurant]
+        b: {name: "Nature", searchQuery:"forests", id: ["9927003, 9362015", "8099025", "8099013", "9362030", "8099019"]}, //[Zoo, Forest Area, Hill, Reservoir, Natural Recreation Attraction, Lagoon ] 
     },
 ]
 
@@ -43,8 +39,8 @@ function updateElementWithRandomPhoto(searchTerm, elementId) {
 }
 
 function updateOptionPhotos(index) {
-    updateElementWithRandomPhoto(OPTIONS[index].a, OPTION_A_ID);
-    updateElementWithRandomPhoto(OPTIONS[index].b, OPTION_B_ID);
+    updateElementWithRandomPhoto(OPTIONS[index].searchQuery, OPTION_A_ID);
+    updateElementWithRandomPhoto(OPTIONS[index].searchQuery, OPTION_B_ID);
 }
 
 function handleImageClick(option, index) {
