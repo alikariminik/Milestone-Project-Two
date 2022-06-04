414e7d00333eabbe2a58ad06ce1dbbef1600d2e0
alikariminik <alirkariminik@gmail.com>
2022-06-04 13:13:50 +0000

    identified issue with updateOptionPhotos which prevented images from loading

--- scripts/javascript/script.js
+++ scripts/javascript/script.js
@@ -4,22 +4,54 @@ const OPTION_A_ID = "optionA";
 const OPTION_B_ID = "optionB";
 
 const OPTIONS = [{
-        a: { name: "Beaches", searchQuery:"beach resort", id: ["9357", "9937002"]}, //[Beach, Beach Club]
-        b: { name: "Mountain Range", searchQuery:"mountain range", id: ["7302002", "8099002", "9935", "9362026"]}, //[Mountain Bike Trail, Mountain Peak, Mountain Pass, Ski Resort]
+        a: {
+            name: "Beaches",
+            searchQuery: "beach resort",
+            id: ["9357", "9937002"]
+        }, //[Beach, Beach Club]
+        b: {
+            name: "Mountain Range",
+            searchQuery: "mountain range",
+            id: ["7302002", "8099002", "9935", "9362026"]
+        }, //[Mountain Bike Trail, Mountain Peak, Mountain Pass, Ski Resort]
     },
     {
-        a: { name: "Adventurous", searchQuery:"adventure activities", id: ["7305", "9371", "7302004", "9902003"]}, //[Adventure Sports Facility, Water Sport, Hiking Trail, Amusement Park]
-        b: { name: "Relaxing", searchQuery:"massage", id: ["9378005", "7338", "7314005"]}, // [Sauna, Solarium & Massage, Swimming Pool, Resort]
+        a: {
+            name: "Adventurous",
+            searchQuery: "adventure activities",
+            id: ["7305", "9371", "7302004", "9902003"]
+        }, //[Adventure Sports Facility, Water Sport, Hiking Trail, Amusement Park]
+        b: {
+            name: "Relaxing",
+            searchQuery: "massage",
+            id: ["9378005", "7338", "7314005"]
+        }, // [Sauna, Solarium & Massage, Swimming Pool, Resort]
     },
     {
-        a: {name: "Historical", searchQuery:"historic monuments", id: ["7317", "7376003", "9362007", "9362004", "9362002" ]}, //[Museum, Monument, Memorial, Historic Site, Battlefield ]
-        b: {name: "Party", searchQuery:"nightlife", id: ["9379002", "9379006", "9379004", "9379008", "9379009" ]}, // [Disco Club, Cocktail Bar, Bar, Jazz Club, Comedy Club ]
+        a: {
+            name: "Historical",
+            searchQuery: "historic monuments",
+            id: ["7317", "7376003", "9362007", "9362004", "9362002"]
+        }, //[Museum, Monument, Memorial, Historic Site, Battlefield ]
+        b: {
+            name: "Party",
+            searchQuery: "nightlife",
+            id: ["9379002", "9379006", "9379004", "9379008", "9379009"]
+        }, // [Disco Club, Cocktail Bar, Bar, Jazz Club, Comedy Club ]
     },
     {
-        a: {name: "Cultural", searchQuery:"cultural arts", id: ["7318005", "7318004", "7339002", "7339003", "7339004", "7315" ]}, //[Theater, Opera House, Church, Mosque, Synagog, Restaurant]
-        b: {name: "Nature", searchQuery:"forests", id: ["9927003, 9362015", "8099025", "8099013", "9362030", "8099019"]}, //[Zoo, Forest Area, Hill, Reservoir, Natural Recreation Attraction, Lagoon ] 
+        a: {
+            name: "Cultural",
+            searchQuery: "cultural arts",
+            id: ["7318005", "7318004", "7339002", "7339003", "7339004", "7315"]
+        }, //[Theater, Opera House, Church, Mosque, Synagog, Restaurant]
+        b: {
+            name: "Nature",
+            searchQuery: "forests",
+            id: ["9927003, 9362015", "8099025", "8099013", "9362030", "8099019"]
+        }, //[Zoo, Forest Area, Hill, Reservoir, Natural Recreation Attraction, Lagoon ] 
     },
-]
+];
 
 window.onload = function () {
     let launchQuestions = document.getElementById('launch-questions');
@@ -39,8 +71,8 @@ function updateElementWithRandomPhoto(searchTerm, elementId) {
 }
 
 function updateOptionPhotos(index) {
-    updateElementWithRandomPhoto(OPTIONS[index].searchQuery, OPTION_A_ID);
-    updateElementWithRandomPhoto(OPTIONS[index].searchQuery, OPTION_B_ID);
+    updateElementWithRandomPhoto(OPTIONS[index].a.searchQuery,OPTION_A_ID);
+    updateElementWithRandomPhoto(OPTIONS[index].b.searchQuery, OPTION_B_ID);
 }
 
 function updateQuestionText(index, id) {
@@ -54,35 +86,36 @@ function addAnswer(isAnswerA) {
 }
 
 function handleOptionClick(isOptionA) {
-    addAnswer(isOptionA);
-    index - index + 1; 
-    updateOptionPhotos(index);
-    updateQuestionText(index, "question-generation-field");
-    $("#back-button").html(`<button" class="back-button"> back to last question <i class="fa-solid fa-arrow-left"></i></button>`)
+    if (index === OPTIONS.length - 1) {
+        handleFinalOption(isOptionA);
+    } else {
+        addAnswer(isOptionA);
+        index = index + 1;
+        updateOptionPhotos(index);
+        updateQuestionText(index, "question-generation-field");
+        $("#back-button").html(`<button" class="back-button"> back to last question <i class="fa-solid fa-arrow-left"></i></button>`)
+    }
 }
 
 
-function initialiseImages(index) {
+function handleFinalOption(isAnswerA) {
+    addAnswer(isAnswerA);
+    let quiz = document.getElementById("quiz");
+    quiz.classList.add("hidden");
+}
+
+function initialiseImages() {
     let optionAImage = document.getElementById(OPTION_A_ID);
     let optionBImage = document.getElementById(OPTION_B_ID);
 
-    updateOptionPhotos(index);
+    updateOptionPhotos(0);
+    updateQuestionText(0, "question-generation-field");
 
-    optionAImage.addEventListener("click", () => {
-        handleImageClick(OPTIONS[index].a, index);
-        index = index + 1;
-    })
-
-    optionBImage.addEventListener("click", () => {
-        handleImageClick(OPTIONS[index].b, index);
-        index = index + 1;
-    })
+    optionAImage.addEventListener("click", () => handleOptionClick(true));
+    optionBImage.addEventListener("click", () => handleOptionClick(true));
 }
 
-
-
-function startAgain(event) {
-
-}
-
-module.exports = initialiseImages;
+module.exports = {
+    OPTIONS,
+    initialiseImages
+};
--- scripts/javascript/startQuizButton.js
+++ scripts/javascript/startQuizButton.js
@@ -3,28 +3,14 @@ let index = 0;
 
 function startQuiz (event) {
     removeIntroductionText();
-    initialiseImages(index);
+    initialiseImages();
 
-    
-    updateElementWithRandomPhoto("beaches", OPTION_A_ID, index + 1);
-    updateElementWithRandomPhoto("mountain range", OPTION_B_ID, index + 1);
-
-    document.getElementById("question-generation-field").innerHTML = `<p class="test">Beaches or Mountains?</p>`;
      $("#iw").addClass("images-wrapper");
-     $("#op-a").addClass("left-image-wrapper");
-     $("#op-b").addClass("right-image-wrapper");
-     
-     $("#back-button").html("<button class='back-button'>back to last question <i class='fa-solid fa-arrow-left'></i> </button>");
+
      document.getElementById("back-button").addEventListener("click", previousQuestion);
 
-     let answerA = document.getElementById("optionA");
-     answerA.addEventListener("click", () => {
-       answers.push("optionA");
-       console.log(answers, answers.length);
-     });
-
      $("#reset-button").html("<button class='reset-button'>start again <i class='fa-solid fa-arrow-rotate-right'></i> </button>");
-     document.getElementById("reset-button").addEventListener("click", resetQuestions);   
+     document.getElementById("reset-button").addEventListener("click", () => {console.log(answers, index);resetQuestions(); console.log(answers, index);});   
 }
 
 function removeIntroductionText () {
