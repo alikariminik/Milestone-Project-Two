522e74a2c2b707a6368a3f00a5972a8c66d5acef
alikariminik <alirkariminik@gmail.com>
2022-06-05 15:38:07 +0000

    due to unsplash api generating too many inappropriate results when random pictures were requested, switched the request to specific pre-planned images

--- scripts/javascript/options.js
+++ scripts/javascript/options.js
@@ -7,48 +7,48 @@ const backButton = document.getElementById("back-button");
 const OPTIONS = [{
         a: {
             name: "Beaches",
-            searchQuery: "Sea Beach",
+            searchQuery: "nYwfKdzB_Ds",
             id: ["9357", "9937002"]
         }, //[Beach, Beach Club]
         b: {
             name: "Mountain Range",
-            searchQuery: "Everest",
+            searchQuery: "u5yopQOsFqg",
             id: ["7302002", "8099002", "9935", "9362026"]
         }, //[Mountain Bike Trail, Mountain Peak, Mountain Pass, Ski Resort]
     },
     {
         a: {
             name: "Adventurous",
-            searchQuery: "hiking",
+            searchQuery: "kQu71SCrwns",
             id: ["7305", "9371", "7302004", "9902003"]
         }, //[Adventure Sports Facility, Water Sport, Hiking Trail, Amusement Park]
         b: {
             name: "Relaxing",
-            searchQuery: "Sunbathing",
+            searchQuery: "LmF4eBYs08c",
             id: ["9378005", "7338", "7314005"]
         }, // [Sauna, Solarium & Massage, Swimming Pool, Resort]
     },
     {
         a: {
             name: "Historical",
-            searchQuery: "Big Ben",
+            searchQuery: "jNQK0RnwCSo",
             id: ["7317", "7376003", "9362007", "9362004", "9362002"]
         }, //[Museum, Monument, Memorial, Historic Site, Battlefield ]
         b: {
             name: "Party",
-            searchQuery: "Partying",
+            searchQuery: "MxfcoxycH_Y",
             id: ["9379002", "9379006", "9379004", "9379008", "9379009"]
         }, // [Disco Club, Cocktail Bar, Bar, Jazz Club, Comedy Club ]
     },
     {
         a: {
             name: "Cultural",
-            searchQuery: "Mona Lisa",
+            searchQuery: "7k91OUDYAQ0",
             id: ["7318005", "7318004", "7339002", "7339003", "7339004", "7315"]
         }, //[Theater, Opera House, Church, Mosque, Synagog, Restaurant]
         b: {
             name: "Nature",
-            searchQuery: "forests",
+            searchQuery: "dIMJcgCYZJw",
             id: ["9927003, 9362015", "8099025", "8099013", "9362030", "8099019"]
         }, //[Zoo, Forest Area, Hill, Reservoir, Natural Recreation Attraction, Lagoon ] 
     },
@@ -61,7 +61,7 @@ window.onload = function () {
 
 
 function updateElementWithRandomPhoto(searchTerm, elementId) {
-    const url = `https://api.unsplash.com/photos/random?query=${searchTerm}&orientation=squarish&client_id=${UNSPLASH_API_KEY}`;
+    const url = `https://api.unsplash.com/photos/${searchTerm}?client_id=${UNSPLASH_API_KEY}`;
 
     fetch(url)
         .then((response) => response.json())
