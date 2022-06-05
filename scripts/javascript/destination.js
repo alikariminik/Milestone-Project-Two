25bcc38a29c17cbb1b28f6615cf5020e0d56014f
alikariminik <alirkariminik@gmail.com>
2022-06-05 14:57:42 +0000

    added styling

--- scripts/javascript/destination.js
+++ scripts/javascript/destination.js
@@ -8,24 +8,36 @@ function getIdsForRequest(answers) {
 }
 
 function getDestination(ids) {
-    const tenIds = ids.sort(() => .5 - Math.random()).slice(0,9); // API can only have 10 ids so need to sort
-    const tenCountryCodes = countryCodes.sort(() => .5 - Math.random()).slice(0,9);
+    const tenIds = ids.sort(() => .5 - Math.random()).slice(0, 9); // API can only have 10 ids so need to sort
+    const tenCountryCodes = countryCodes.sort(() => .5 - Math.random()).slice(0, 9);
     const url = `https://api.tomtom.com/search/2/categorySearch/places.json?key=${TOMTOM_API_KEY}&limit=100&ofs=1900&rcategorySet=${tenIds.join}countrySet=${tenCountryCodes.join(",")}`;
 
     fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
             if (data.results.length > 0) {
-                location = data.results[getRandomNumber(data.results.length) - 1].localname.country;
-                updateElementWithRandomPhoto(location, "destinationImgId")
+                const location = data.results[getRandomNumber(data.results.length) - 1];
+                document.getElementById("destination").innerHTML = location;
+                // localname
+                // country
+                //updateElementWithRandomPhoto(location, "destinationImgId")
             }
-    })
-    .catch((err) => console.error("error getting image"));
-}
+        })
+        .catch((err) => console.error("error getting image"));
+};
 
 function getRandomNumber(n) {
     return Math.round(Math.random() * n);
-  }
-  
-  module.exports = { getDestination, getIdsForRequest }
+}
+
+module.exports = {
+    getDestination,
+    getIdsForRequest
+}
+
+//     console.log(location)
+//     let destinationResults = document.getElementById("destination");
+//     destinationResults.innerHTML(`<h2 class="container">Based off your answers, we think you would like</h2>
+//     <div class="container"></div>`)
+// updateElementWithRandomPhoto(location, "destinationImgId")
